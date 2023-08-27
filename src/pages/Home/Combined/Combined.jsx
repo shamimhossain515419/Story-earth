1627309540f23a8ba401689b920963bc598a7750
Shamimusman515419 <shamimhossain01617@gmail.com>
2023-08-27 22:23:21 +0600

    swiper

--- src/pages/Home/Combined/Combined.jsx
+++ src/pages/Home/Combined/Combined.jsx
@@ -1,5 +1,9 @@
 import Container from "../../../Component/Container/Container";
 import './Combined.css'
+import Slider1 from "./Slider1";
+import Slider2 from "./Slider2";
+import switchimage from '../../../../public/phone/switch.6f2ecb8a.svg';
+
 const Combined = () => {
      return (
           <div className="CombinedBg mt-10  md:mt-40">
@@ -10,11 +14,23 @@ const Combined = () => {
 
                          <div className=" mt-10">
                               <h1 className=" text-center text-lg md:text-xl xl:text-3xl 2xl:text-4xl font-bold mt-10"> The First-Ever Combined Custodial & Non-Custodial Story earth</h1>
-
-
-                              <p className=" max-w-[1000px] mx-auto text-center text-base md:text-lg xl:text-xl font-medium my-10"> With Cwallet, you will get the security and privacy of non-custodial wallets & the convenience and efficacy of custodial wallets in one place. You can easily manage and trade 800+ crypto assets in one app with flexibility, capability, and creativity.</p>
+                              <p className=" max-w-[1000px] mx-auto text-center text-base  md:text-lg font-medium my-10"> With Cwallet, you will get the security and privacy of non-custodial wallets & the convenience and efficacy of custodial wallets in one place. You can easily manage and trade 800+ crypto assets in one app with flexibility, capability, and creativity.</p>
                          </div>
 
+                    </div>
+
+                    <div className=" max-w-[1000px] mx-auto">
+                         <div className=" my-10 flex justify-between gap-6">
+                              <div className=" max-w-[300px]">
+                                   <Slider1></Slider1>
+                              </div>
+                              <div className="   flex justify-center items-center">
+                                   <img className=" hidden md:block w-16 h-16 " src={switchimage} alt="" />
+                              </div>
+                              <div className=" max-w-[300px]">
+                                   <Slider2></Slider2>
+                              </div>
+                         </div>
                     </div>
 
                </Container>
---
+++ src/pages/Home/Combined/Slider2.jsx
@@ -0,0 +1,68 @@
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import image1 from '../../../../public/phone/custodialSub.48ef2441.png'
+import image2 from '../../../../public/phone/nonCustodialSub.134b53cc.png'
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+export default function Slider2() {
+     return (
+          <>
+               <div className=' max-w-[1000px] mx-auto  '>
+
+                    <div >
+                         <div>
+                              <Swiper
+                                   spaceBetween={30}
+                                   centeredSlides={true}
+                                   autoplay={{
+                                        delay: 2500,
+                                        disableOnInteraction: false,
+                                   }}
+                                   loop={true}
+                                   
+                                  
+                                   modules={[Autoplay, Pagination, Navigation]}
+                                   className="mySwiper"
+                              >
+
+                                   <SwiperSlide>
+                                        <div className='  rounded-xl overflow-hidden cursor-pointer  w-full  max-h-[600px]'>
+                                             <img className=' w-full h-full' src={image2} alt="" />
+                                        </div>
+                                   </SwiperSlide>
+                                   <SwiperSlide>
+                                        <div className='  rounded-xl overflow-hidden cursor-pointer  w-full  max-h-[600px]'>
+                                             <img className=' w-full h-full' src={image1} alt="" />
+                                        </div>
+                                   </SwiperSlide>
+                                   <SwiperSlide>
+                                        <div className='  rounded-xl overflow-hidden cursor-pointer  w-full  max-h-[600px]'>
+                                             <img className=' w-full h-full' src={image2} alt="" />
+                                        </div>
+                                   </SwiperSlide>
+                                   <SwiperSlide>
+                                        <div className='  rounded-xl overflow-hidden cursor-pointer  w-full  max-h-[600px]'>
+                                             <img className=' w-full h-full' src={image1} alt="" />
+                                        </div>
+                                   </SwiperSlide>
+
+
+
+                              </Swiper>
+                         </div>
+
+
+                    </div>
+               </div>
+          </>
+     );
+}
